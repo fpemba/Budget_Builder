f6a8c969c15671f3c196eb9b3ecce699a670fb87
fpemba <fatuma.pemba@yahoo.com>
2020-05-13 19:40:45 -0400

    created basic company email generator

---
+++ Downloads/EmailApplication/src/org/wecancodeit/EmailApp.java
@@ -0,0 +1,14 @@
+package org.wecancodeit;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+        Email em1 = new Email("Fatuma","Pemba");
+        System.out.println(em1.showInfo());
+
+
+
+
+    }
+}
